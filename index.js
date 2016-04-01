8ff1e25cffd171696ad7758666af5e4b98584514
hezedu <hezedu@hotmail.com>
2016-04-02 06:23:23 +0800

    增加了wrap选项

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function server(tpl_path, opts) {
     //默认目录
     tpl_path = path.join(process.cwd(), 'jade_compiled');
   }
-
   opts = opts || {};
   opts.tpl_path = tpl_path; //模版目录
   //缓存期限 默认0
@@ -20,7 +19,8 @@ function server(tpl_path, opts) {
   //是否压缩 默认:测试环境 否 , 生产环境 是
   opts.uglify = opts.uglify === undefined ?
     (process.env.NODE_ENV === 'production' ? true : false) : opts.watch;
-
+  opts.wrap = opts.wrap || 'define(function(){return $COMPILED$})';
+  opts.wrap = opts.wrap.split('$COMPILED$');
   //监控模版目录
   if(opts.watch){
     watch.watchTree(tpl_path,{persistent: true, interval: 1000}, function (fileName, curr, prev) {
@@ -33,25 +33,21 @@ function server(tpl_path, opts) {
     });
   }
   var splitKey = '//-COMPONENT ';
+  var wrap_pre = opts.wrap[0];
+  var wrap_back = opts.wrap[1];
+  opts.wrap = null;
+  var header = {
+    'Content-Type':'application/javascript'
+  }
+  if (opts.maxAge > 0) {
+    header['Cache-Control'] = "max-age=" + opts.maxAge;
+  }
+
   return function(req, res, next) {
-
     var real_path = path.join(tpl_path, req.path);
-
-
     var cache_key = real_path + ':client';
-    
-    //console.log('cache_key', cache_key);
     if (!jade.cache[cache_key]) { //如果没有缓存的话.
-      var is_first = (undefined === jade.cache[cache_key]);
-
-      try {
-        var doc = fs.readFileSync(real_path, 'utf-8');
-      } catch (err) {
-        err.name = 'express-jade-compiled ' + err.name;
-        return next(err);
-      }
-
-
+      var doc = fs.readFileSync(real_path, 'utf-8');
       doc = doc.split(splitKey);
       var dump = '';
       if (doc.length === 1) {
@@ -59,15 +55,12 @@ function server(tpl_path, opts) {
         dump = jade.compileClient(dump, {
           name: 'tpl'
         });
-
         if (opts.uglify) {
           dump = uglify.minify(dump, {
             fromString: true
           }).code;
         }
-
       } else {
-
         var dump = [];
         for (var i = 1, len = doc.length; i < len; i++) {
           var curr = doc[i];
@@ -79,7 +72,6 @@ function server(tpl_path, opts) {
           v = jade.compileClient(v, {
             name: 'tpl' + i
           });
-
           if (opts.uglify) {
             v = uglify.minify(v, {
               fromString: true
@@ -87,39 +79,17 @@ function server(tpl_path, opts) {
           }
           dump.push('"' + key + '":' + v);
         }
-
         dump = dump.join(',');
         dump = '{' + dump + '}';
       }
-
       jade.cache[cache_key] = dump;
-
-/*      if (is_first && opts.watch) {
-        var is_change = false;
-        fs.watch(real_path, function() {
-          if (!is_change) {
-            is_change = true;
-            jade.cache[cache_key] = null;
-            //console.log('缓存已销毁!');
-
-            setTimeout(function() {
-              is_change = false
-            }, 500);
-          }
-        });
-      }*/
     }
-    res.setHeader('Content-Type', 'application/javascript');
-    if (opts.maxAge > 0) {
-      res.setHeader('Cache-Control', "max-age=" + opts.maxAge);
-    }
-
-    var outstr = 'define(function(){return ' + jade.cache[cache_key] + '})'
+    res.set(header);
+    var outstr = wrap_pre + jade.cache[cache_key] + wrap_back
     res.send(outstr);
   }
 }
 
 server.jade_runtime_min_path = path.join(__dirname, 'jade-runtime-min.js');
-
 
 module.exports = server;
